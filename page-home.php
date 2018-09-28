6065ffad16f5367bec2563e1b509d9616a767341
DuckDivers <howard@duckdiverllc.com>
2018-09-28 15:53:30 -0400

    Make homepage fluid

--- page-home.php
+++ page-home.php
@@ -1,36 +1,34 @@
 <?php
 /**
-* Template Name: Home Page
-*
-* @package Duck Diver Framework 1.1
-*/
-get_header();?>
-<main id="main" class="home-main" role="main">
-<?php get_template_part( 'template-parts/content', 'slider' ); ?>
-	<div id="homepage-content" class="container-fluid">
-		<div id="main-content" class="row">
-            <div class="col">
-                <?php while ( have_posts() ) : the_post(); ?>
-                
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    
-                    <div class="entry-content">
-                        <?php the_content(); ?>
-                        <?php
-                            wp_link_pages( array(
-                                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dd_theme' ),
-                                'after'  => '</div>',
-                            ) );
-                        ?>
-                    </div><!-- .entry-content -->
-                    
-                    </article><!-- #post-## -->
-                <?php endwhile; // End of the loop. ?>
-            </div>
+ * Template Name: Home Page
+ *
+ * @package Duck Diver Framework 1.1
+ */
+get_header();
+?>
+<main id="main" class="home-main col" role="main">
+    <?php get_template_part( 'template-parts/content', 'slider' ); ?>
 
+    <?php while ( have_posts() ) : the_post(); ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+        <div class="entry-content">
+            <?php the_content(); ?>
+            <?php
+            wp_link_pages( array(
+                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dd_theme' ),
+                'after' => '</div>',
+            ) );
+            ?>
         </div>
-	</div>
-<?php if (get_theme_mod('slider_active')) {
+        <!-- .entry-content -->
+
+    </article>
+    <!-- #post-## -->
+    <?php endwhile; // End of the loop. ?>
+
+    <?php if (get_theme_mod('slider_active')) {
 			$delay = get_theme_mod('slider_delay'); }
 		else {
 			$delay = 'false';
@@ -39,11 +37,11 @@ get_header();?>
 </main>
 
 <script type="text/javascript">
-	// Carousel Init
-	jQuery(document).ready(function($) {
-		$('.carousel').carousel({
-			interval: <?php echo $delay;?>		
-		});
-	});
+    // Carousel Init
+    jQuery( document ).ready( function ( $ ) {
+        $( '.carousel' ).carousel( {
+            interval: <?php echo $delay;?>
+        } );
+    } );
 </script>
 <?php get_footer(); ?>
