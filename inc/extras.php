f46979b24d69a1612dc58445515be139e71d91c7
Howard <howard@duckdiverllc.com>
2018-10-11 10:03:21 -0400

    Add Font Size to Tiny MCE

--- inc/extras.php
+++ inc/extras.php
@@ -48,3 +48,19 @@ function dd_get_sidebar_position(){
     }
     return $sbpos;
 }
+//Add Font Size to WP Visual Editor
+function dd_wp_editor_fontsize_filter( $options ) {
+	array_shift( $options );
+	array_unshift( $options, 'fontsizeselect');
+	return $options;
+}
+
+add_filter('mce_buttons_2', 'dd_wp_editor_fontsize_filter');
+// Customize mce editor font sizes
+if ( ! function_exists( 'wpex_mce_text_sizes' ) ) {
+    function wpex_mce_text_sizes( $initArray ){
+        $initArray['fontsize_formats'] = "9px 10px 12px 13px 14px 16px 18px 21px 24px 28px 32px 36px";
+        return $initArray;
+    }
+}
+add_filter( 'tiny_mce_before_init', 'wpex_mce_text_sizes' ); 
