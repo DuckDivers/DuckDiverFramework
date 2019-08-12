82b3cdae3d3535404b806321c3c791268403236e
Howard <howard@duckdiverllc.com>
2019-08-12 17:32:11 -0400

    Update page-fullwidth-fluid.php

--- page-fullwidth-fluid.php
+++ page-fullwidth-fluid.php
@@ -10,7 +10,7 @@ get_header();
 
 <div id="full-width-container" class="container-fluid">
     <div class="row">
-        <main id="main" class="site-main col" role="main">
+        <main id="main" class="site-main col p-0" role="main">
             
             <?php while ( have_posts() ) : the_post(); ?>
             
