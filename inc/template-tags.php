6a340d97631736c0b42e64ea6948432c1134e5ce
duckdivers <howard@duckdiverllc.com>
2020-10-02 11:54:25 -0400

    Fix single post featured image

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -11,33 +11,41 @@ if ( ! function_exists( 'dd_theme_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
  */
-function dd_theme_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
+ function dd_theme_posted_on() {
+ 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+ 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+ 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+ 	}
 
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
+     $modified_time = '<time class="updated" datetime="%1$s">%2$s</time>';
 
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'dd_theme' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
+ 	$time_string = sprintf( $time_string,
+ 		esc_attr( get_the_date( 'c' ) ),
+ 		esc_html( get_the_date() )
+       );
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'dd_theme' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
+     $modified_string = sprintf( $modified_time,
+         esc_attr( get_the_modified_date( 'c' ) ),
+ 		esc_html( get_the_modified_date() )
+       );
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+ 	$posted_on = sprintf(
+ 		esc_html_x( 'Posted on %s', 'post date', 'dd_theme' ),
+ 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>');
 
-}
-endif;
+     if ( get_the_date() !== get_the_modified_date() ){
+         $posted_on .= sprintf(
+             esc_html_x( ' Updated on %s', 'post modified date', 'dd_theme'), $modified_string);
+     }
+ 	$byline = sprintf(
+ 		esc_html_x( 'by %s', 'post author', 'dd_theme' ),
+ 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+ 	);
+
+ 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+
+ }
+ endif;
 
 if ( ! function_exists( 'dd_theme_entry_footer' ) ) :
 /**
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -7,9 +7,24 @@
  * @package Duck Diver Theme
  */
 
+$featured_size = (get_theme_mod('dd_featured_post_image')) ? get_theme_mod('dd_featured_post_image') : 'none' ;
+if ($featured_size === 'large') {
+    $featured_class = 'col-12';
+    }
+else if ($featured_size === 'small'){
+    $featured_class = 'col-md-6 offset-md-3';
+    }
+else {
+    $featured_class='d-none';}
+    $post_class = ($featured_size !== 'small') ? 'col-12' : 'col-md-8';
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php if (has_post_thumbnail()){
+        echo '<div class="row"><figure class="featured-thumbnail thumbnail '.$featured_class.'">'.get_the_post_thumbnail($post->ID, 'blog-hero-image', array('class' => 'd-block mx-auto')).'</figure></div>';
+		  }
+    ?>
+
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
