37c0ca8d02c3523ed1e1279a24f08b2749af50a4
duckdivers <howard@duckdiverllc.com>
2021-03-03 13:40:54 -0500

    Filter Comment Form Args

--- inc/functions-strap.php
+++ inc/functions-strap.php
@@ -322,28 +322,28 @@ if (!function_exists('bootstrap_comment_form_defaults')){
         $req = get_option('require_name_email');
         $aria_req = ($req ? " aria-required='true'" : '');
         $defaults['fields'] = array(
-            'author' => '<div class="form-group comment-form-author">' .
-                '<label for="author" class="col-sm-3 control-label">' . __('Name', 'dd_theme') . ($req ? ' <span class="required">*</span>' : '') . '</label> ' .
-                '<div class="col-sm-9">' .
+            'author' => '<div class="form-group comment-form-author row">' .
+                '<label for="author" class="col-md-2 control-label">' . __('Name', 'dd_theme') . ($req ? ' <span class="required">*</span>' : '') . '</label> ' .
+                '<div class="col-md-10">' .
                 '<input id="author" name="author" type="text" value="' . esc_attr($commenter['comment_author']) . '"  class="form-control"' . $aria_req . ' />' .
                 '</div>' .
                 '</div>',
-            'email' => '<div class="form-group comment-form-email">' .
-                '<label for="email" class="col-sm-3 control-label">' . __('Email', 'dd_theme') . ($req ? ' <span class="required">*</span>' : '') . '</label> ' .
-                '<div class="col-sm-9">' .
+            'email' => '<div class="form-group comment-form-email row">' .
+                '<label for="email" class="col-md-2 control-label">' . __('Email', 'dd_theme') . ($req ? ' <span class="required">*</span>' : '') . '</label> ' .
+                '<div class="col-md-10">' .
                 '<input id="email" name="email" type="text" value="' . esc_attr($commenter['comment_author_email']) . '"  class="form-control"' . $aria_req . ' />' .
                 '</div>' .
                 '</div>',
-            'url' => '<div class="form-group comment-form-url">' .
-                '<label for="url" class="col-sm-3 control-label"">' . __('Website', 'dd_theme') . '</label>' .
-                '<div class="col-sm-9">' .
+            'url' => '<div class="form-group comment-form-url row">' .
+                '<label for="url" class="col-md-2 control-label"">' . __('Website', 'dd_theme') . '</label>' .
+                '<div class="col-md-10">' .
                 '<input id="url" name="url" type="text" value="' . esc_attr($commenter['comment_author_url']) . '"  class="form-control" />' .
                 '</div>' .
                 '</div>',
         );
-        $defaults['comment_field'] = '<div class="form-group comment-form-comment">' .
-            '<label for="comment" class="col-sm-3 control-label">' . _x('Comment', 'noun', 'dd_theme') . '</label>' .
-            '<div class="col-sm-9">' .
+        $defaults['comment_field'] = '<div class="form-group comment-form-comment row">' .
+            '<label for="comment" class="col-md-2 control-label">' . _x('Comment', 'noun', 'dd_theme') . '</label>' .
+            '<div class="col-md-10">' .
             '<textarea id="comment" name="comment" aria-required="true" class="form-control" rows="8"></textarea>' .
             '<span class="help-block form-allowed-tags">' . sprintf(__('You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s'), ' <code>' . allowed_tags() . '</code>') . '</span>' .
             '</div>' .
